df2a450075b6e909dc76d2c02a92010af77d04f7
Nicolas Dupouy <nicolasdupouy@gmail.com>
2020-05-01 16:21:34 +0200

    Create a very basic hello REST API

--- README.md
+++ README.md
@@ -2,8 +2,16 @@
 
 The goal of this repository is to store all the codes I want to test in Go
 
-
+# Go
 ## Run
 ```
 $ go run ./hello/main.go
 ```
+
+## Import dependencies
+Get the dependencies imported in the code but missing in the path (it will update the *go.mod* file).
+
+```
+go mod tidy
+```
+
--- hello/main.go
+++ hello/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello friend !")
+	log.Println("Initializing Hello friend API")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", ServeHTTP)
+
+	server := http.Server{
+		Addr:    net.JoinHostPort("", "8080"),
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write a JSON message into the response body
+	message := `{"message": "Hello friend !"}`
+	_, err := w.Write([]byte(message))
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
