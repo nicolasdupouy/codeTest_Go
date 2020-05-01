52e5658fe8dffa9a271ae119cc0363c812b22ef7
Nicolas Dupouy <nicolasdupouy@gmail.com>
2020-05-01 18:18:28 +0200

    Write the same function with strings.Builder{}

--- gopher/gopher.go
+++ gopher/gopher.go
@@ -1,8 +1,20 @@
 package gopher
 
+import "strings"
+
 func Plural(s string, count int) string {
 	if count == 1 {
 		return s
 	}
 	return s + "s"
+}
+
+func PluralWithStringsBuilder(s string, count int) string {
+	if count == 1 {
+		return s
+	}
+	builder := strings.Builder{}
+	builder.WriteString(s)
+	builder.WriteString("s")
+	return builder.String()
 }
--- gopher/gopher_test.go
+++ gopher/gopher_test.go
@@ -2,9 +2,12 @@ package gopher
 
 import "testing"
 
-func TestPlural(t *testing.T) {
-	// Tab containing test cases
-	cases := []struct {
+func GetTestCases() []struct {
+	Input  string
+	Count  int
+	Output string
+} {
+	return []struct {
 		Input  string
 		Count  int
 		Output string
@@ -14,9 +17,25 @@ func TestPlural(t *testing.T) {
 		{Input: "tomate", Count: 3, Output: "tomates"},
 		{Input: "tomate", Count: 9999, Output: "tomates"},
 	}
+}
+
+func TestPlural(t *testing.T) {
+	// Tab containing test cases
+	cases := GetTestCases()
 
 	for _, c := range cases {
 		result := Plural(c.Input, c.Count)
+		if result != c.Output {
+			t.Errorf("expected %s, got %s", c.Output, result)
+		}
+	}
+}
+
+func TestPluralWithStringsBuilder(t *testing.T) {
+	cases := GetTestCases()
+
+	for _, c := range cases {
+		result := PluralWithStringsBuilder(c.Input, c.Count)
 		if result != c.Output {
 			t.Errorf("expected %s, got %s", c.Output, result)
 		}
